49cf71fb25348e11506f717340e5fabb67cdd39c
Angel Travieso <angeltraviesoc@gmail.com>
2022-04-26 17:02:52 -0400

    construcción menú interactivo

---
+++ app.js
@@ -0,0 +1,27 @@
+require('colors');
+
+const { inquirerMenu, pausa } = require('./helpers/inquirer');
+
+console.clear();
+
+const main = async() => {
+
+    console.log('Hola mundo');
+
+    let opt = '';
+
+    do {
+        opt = await inquirerMenu();
+        
+        console.log({ opt });
+
+        await pausa();
+
+    } while (opt !== '0');
+
+    // pausa();
+
+}
+
+
+main();
