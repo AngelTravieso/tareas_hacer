73de2017eede366051ffc1815ad82c72bfbb09cc
AngelTravieso <angeltraviesoc@gmail.com>
2022-05-01 13:52:23 -0400

    crear y listar tareas

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 require('colors');
-const { inquirerMenu, pausa } = require('./helpers/inquirer');
+const { inquirerMenu, pausa, leerInput } = require('./helpers/inquirer');
 const Tareas = require('./models/tareas');
 
 const main = async() => {
 
     let opt = '';
+    const tareas = new Tareas();
 
     do {
         opt = await inquirerMenu();
-        console.log({ opt });
+
+        switch(opt) {
+            case '1':
+                // Crear opocion
+                const desc = await leerInput('Descripción:');
+                tareas.crearTarea( desc );
+
+            break;
+            case '2':
+                // Listar las opciones
+                console.log(tareas._listado);
+            break;
+        }
 
         await pausa(); 
 
